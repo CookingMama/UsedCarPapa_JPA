8a8e7f479961be4fc5e17403ef3d3c2dcae8a939
laon0125 <jinu0125@gmail.com>
2023-02-21 12:28:30 +0900

    conflict4

--- src/main/java/com/mamaAndPapa/usedCarPapa/repository/SellingCarRepository.java
+++ src/main/java/com/mamaAndPapa/usedCarPapa/repository/SellingCarRepository.java
@@ -7,5 +7,4 @@ import java.util.List;
 import java.util.Optional;
 
 public interface SellingCarRepository extends JpaRepository<SellingCar, Long> {
-    List<SellingCar.SellingCarMapping> findAllBy();
 }
