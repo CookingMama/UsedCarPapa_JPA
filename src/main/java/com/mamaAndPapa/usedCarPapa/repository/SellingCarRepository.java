c1d89bf3ed2032bc66133881fef2e2824551c0a9
laon0125 <jinu0125@gmail.com>
2023-02-21 17:34:36 +0900

    page implemented

--- src/main/java/com/mamaAndPapa/usedCarPapa/repository/SellingCarRepository.java
+++ src/main/java/com/mamaAndPapa/usedCarPapa/repository/SellingCarRepository.java
@@ -1,12 +1,10 @@
 package com.mamaAndPapa.usedCarPapa.repository;
 
 import com.mamaAndPapa.usedCarPapa.domain.entity.SellingCar;
-import com.mamaAndPapa.usedCarPapa.domain.response.FindAllSellingCarResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface SellingCarRepository extends JpaRepository<SellingCar, Long> {
-     List<SellingCar> findAll();
+     Page<SellingCar> findAllByOrderByCreateAt(Pageable pageable);
 }
