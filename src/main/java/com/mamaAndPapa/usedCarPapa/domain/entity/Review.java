e1a5ed43f313c30ba49b90c5d6a3104b78c44482
laon0125 <jinu0125@gmail.com>
2023-02-21 19:24:13 +0900

    Entity Created

---
+++ src/main/java/com/mamaAndPapa/usedCarPapa/domain/entity/Review.java
@@ -0,0 +1,25 @@
+package com.mamaAndPapa.usedCarPapa.domain.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    @JoinColumn(name = "selling_car_id")
+    private SellingCar sellingCarId;
+    private String review;
+    private Integer rating;
+}
